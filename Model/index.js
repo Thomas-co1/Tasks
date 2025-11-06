10967cb9d137074d6e4ead06e72d4fb30b23027a
Thomas-co1 <tcollignon.74@gmail.com>
2025-11-06 15:01:05 +0100

    changement index et ignore

--- Model/index.js
+++ Model/index.js
@@ -9,10 +9,11 @@ User.hasMany(Task);
 Tag.belongsToMany(Task, { through: 'TaskTags' });
 Task.belongsToMany(Tag, { through: 'TaskTags' });
 
-sequelize.sync({ alter: true });
+//sequelize.sync({ alter: true });
 
 module.exports = {
     'Task': Task,
     'User': User,
-    'Tag': Tag
+    'Tag': Tag,
+    sequelize:sequelize
 };
